47f5b18d79d77a7065ebd3825977e3d29ad82d18
Bogdan Enache <bogdan@pop-os.localdomain>
2023-05-03 16:08:04 +0300

    Complete test suite and implementation for the AppointmentsDayView

--- src/AppointmentsDayView.js
+++ src/AppointmentsDayView.js
@@ -6,9 +6,38 @@ const appointmentTimeOfDay = (startAt) => {
 };
 
 export const Appointment = (props) => {
-   const { customer } = props;
+   const { startsAt, customer, stylist, service, notes } = props;
 
-   return <div>{customer.firstName}</div>;
+   return (
+      <div id="appointmentView">
+         <h3>Today's appointment at {appointmentTimeOfDay(startsAt)}</h3>
+
+         <table>
+            <tbody>
+               <tr>
+                  <td>Customer</td>
+                  <td>{`${customer.firstName} ${customer.lastName}`}</td>
+               </tr>
+               <tr>
+                  <td>Phone number</td>
+                  <td>{customer.phoneNumber}</td>
+               </tr>
+               <tr>
+                  <td>Stylist</td>
+                  <td>{stylist}</td>
+               </tr>
+               <tr>
+                  <td>Service</td>
+                  <td>{service}</td>
+               </tr>
+               <tr>
+                  <td>Notes</td>
+                  <td>{notes}</td>
+               </tr>
+            </tbody>
+         </table>
+      </div>
+   );
 };
 
 export const AppointmentsDayView = (props) => {
--- test/AppointmentsDayView.test.js
+++ test/AppointmentsDayView.test.js
@@ -11,22 +11,116 @@ describe("Appointment", () => {
       document.body.replaceChildren(container);
    });
 
+   const blankCustomer = {
+      firstName: "",
+      lastName: "",
+      phoneNumber: ""
+   }
+
    const render = (component) => {
       act(() => {
          ReactDOM.createRoot(container).render(component);
       });
    };
 
+   const appointmentTable = () => {
+      return document.querySelector("#appointmentView > table");
+   }
+
+   it("renders a div with the right id", () => {
+      render(<Appointment customer={blankCustomer} />);
+      expect(document.querySelector("div#appointmentView")).not.toBeNull();
+   })
+
+   it("renders an h3 heading to display the appointment time", () => {
+      const today = new Date();
+      const startsAt = today.setHours(12, 0);
+
+      render(<Appointment startsAt={startsAt} customer={blankCustomer} />)
+
+      const heading = document.querySelector("h3");
+      expect(heading).not.toBeNull();
+      expect(heading.textContent).toEqual("Today's appointment at 12:00")
+   });
+
+   it("renders a table", () => {
+      render(<Appointment customer={blankCustomer} />);
+      const table = document.querySelector("#appointmentView > table");
+      expect(appointmentTable()).not.toBeNull();
+   })
+
    it("renders the customer first name", () => {
       const customer = { firstName: "Ashley" };
+
       render(<Appointment customer={customer} />);
-      expect(document.body.textContent).toContain("Ashley");
+      expect(appointmentTable().textContent).toContain(
+         "Ashley"
+      );
    });
 
    it("renders another customer first name", () => {
       const customer = { firstName: "Jordan" };
+
       render(<Appointment customer={customer} />);
       expect(document.body.textContent).toContain("Jordan");
+   });
+
+   it("renders the customer last name", () => {
+      const customer = { lastName: "Bartley" };
+
+      render(<Appointment customer={customer} />);
+      expect(appointmentTable().textContent).toContain("Bartley");
+   });
+
+   it("renders another customer last name", () => {
+      const customer = { lastName: "Smith" };
+
+      render(<Appointment customer={customer} />);
+      expect(appointmentTable().textContent).toContain("Smith");
+   })
+
+   it("renders the customer phone number", () => {
+      const customer = { phoneNumber: "333 123-1234" };
+
+      render(<Appointment customer={customer} />)
+      expect(appointmentTable().textContent).toContain("333 123-1234");
+   })
+
+   it("renders another customer phone number", () => {
+      const customer = { phoneNumber: "555 555-5555" };
+
+      render(<Appointment customer={customer} />);
+      expect(appointmentTable().textContent).toContain("555 555-5555");
+   });
+
+   it("renders the stylist name", () => {
+      render(<Appointment customer={blankCustomer} stylist="Michaela" />);
+      expect(appointmentTable().textContent).toContain("Michaela");
+   });
+
+   it("renders another stylist name", () => {
+      render(<Appointment customer={blankCustomer} stylist="Sami" />);
+      expect(appointmentTable().textContent).toContain("Sami");
+   });
+
+   it("renders the saloon service", () => {
+      render(<Appointment customer={blankCustomer} service="Beard trim" />);
+      expect(appointmentTable().textContent).toContain("Beard trim");
+   });
+
+   it("renders another saloon service", () => {
+      render(<Appointment customer={blankCustomer} service="Extensions" />);
+      expect(appointmentTable().textContent).toContain("Extensions");
+   });
+
+   it("renders the appointment notes", () => {
+      render(<Appointment customer={blankCustomer} notes="abcdef" />);
+      expect(appointmentTable().textContent).toContain("abcdef");
+   });
+
+   it("renders another appointment notes", () => {
+      render(<Appointment customer={blankCustomer} notes="ghijkl" />);
+      expect(appointmentTable().textContent).toContain("ghijkl");
    });
 });
 
